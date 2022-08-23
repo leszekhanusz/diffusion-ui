0ca86108b853179f33d09b98ed87eab378b90b3e
Hanusz Leszek <leszek.hanusz@gmail.com>
2022-08-23 20:18:33 +0200

    Adding the pug templating language

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,6 @@
-<template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+<template lang="pug">
+div(class="about")
+  h1 This is an about page with pug
 </template>
 
 <style>
