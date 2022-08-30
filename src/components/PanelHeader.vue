1f17bb3bbb5230ae9b5d9347f1cc87c4e207383d
Hanusz Leszek <leszek.hanusz@gmail.com>
2022-08-30 22:50:21 +0200

    Put model selection in left sidebar

---
+++ src/components/PanelHeader.vue
@@ -0,0 +1,10 @@
+<script setup>
+import Button from "primevue/button";
+import { useUIStore } from "@/stores/ui";
+
+const ui = useUIStore();
+</script>
+
+<template lang="pug">
+Button.p-button-secondary(label="Parameters", @click="ui.showLeftPanel")
+</template>
---
+++ src/views/LeftPanelView.vue
@@ -0,0 +1,17 @@
+<script setup>
+import Sidebar from "primevue/sidebar";
+import Button from "primevue/button";
+import BackendSelector from "@/components/BackendSelector.vue";
+import { useUIStore } from "@/stores/ui";
+
+const ui = useUIStore();
+</script>
+
+<template lang="pug">
+Sidebar.p-sidebar-lg(:visible="ui.left_panel_visible", :showCloseIcon="false")
+
+  template(#header)
+    Button.p-button-secondary(label="Close", @click="ui.hideLeftPanel")
+      | Close
+  BackendSelector
+</template>
