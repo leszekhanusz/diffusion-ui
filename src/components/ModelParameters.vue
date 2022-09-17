de7556f7617a2a91a8d4ab50529dcd9e3ef76107
Hanusz Leszek <leszek.hanusz@gmail.com>
2022-09-17 04:49:22 +0200

    Put access code input in model info tab

--- src/components/ModelParameters.vue
+++ src/components/ModelParameters.vue
@@ -4,6 +4,12 @@ import InputText from "primevue/inputtext";
 import { useBackendStore } from "@/stores/backend";
 
 const backend = useBackendStore();
+
+/*
+        span.p-float-label
+          InputText(type="text", id="input.id", v-model="input.value", :title="input.description")
+          label(for="input.id") {{ input.label }}
+          */
 </script>
 
 <template lang="pug">
@@ -14,10 +20,12 @@ template(v-if="backend.current")
         InputSlider(:label="input.label" v-model="input.value" :min="input.validation.min" :max="input.validation.max" :step="1" :title="input.description")
       template(v-if="input.type == 'float'")
         InputSlider(:label="input.label" v-model="input.value" :min="input.validation.min" :max="input.validation.max" :step="input.step" :title="input.description")
-      template(v-if="input.type == 'text' && input.id !=='prompt'")
-        span.p-float-label
-          InputText(type="text", id="input.id", v-model="input.value", :title="input.description")
-          label(for="input.id") {{ input.label }}
+      template(v-if="input.type == 'text' && input.id !=='prompt' && input.id !=='access_code'")
+        .field.grid(:title="input.description")
+          label.col-fixed(:for="'input_' + input.id" style="min-width: 150px; margin:auto")
+            span {{ input.label }}
+          .col
+            InputText.min-w-full(type="text", :id="'input_' + input.id", v-model="input.value")
 </template>
 
 <style scoped>
