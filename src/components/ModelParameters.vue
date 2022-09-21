6ff476771e97f64744382b12c0f3bc88cbdb96dd
Hanusz Leszek <leszek.hanusz@gmail.com>
2022-09-21 04:21:53 +0200

    Add InputSwitch component for boolean parameters

--- src/components/ModelParameters.vue
+++ src/components/ModelParameters.vue
@@ -1,15 +1,10 @@
 <script setup>
 import InputSlider from "@/components/InputSlider.vue";
+import InputSwitch from "primevue/inputswitch";
 import InputText from "primevue/inputtext";
 import { useBackendStore } from "@/stores/backend";
 
 const backend = useBackendStore();
-
-/*
-        span.p-float-label
-          InputText(type="text", id="input.id", v-model="input.value", :title="input.description")
-          label(for="input.id") {{ input.label }}
-          */
 </script>
 
 <template lang="pug">
@@ -20,12 +15,15 @@ template(v-if="backend.current")
         InputSlider(:label="input.label" v-model="input.value" :min="input.validation.min" :max="input.validation.max" :step="input.step ? input.step: 1" :title="input.description")
       template(v-if="input.type == 'float'")
         InputSlider(:label="input.label" v-model="input.value" :min="input.validation.min" :max="input.validation.max" :step="input.step" :title="input.description")
-      template(v-if="input.type == 'text' && input.id !=='prompt' && input.id !=='access_code'")
+      template(v-if="(input.type == 'text' || input.type == 'boolean') && input.id !=='prompt' && input.id !=='access_code'")
         .field.grid(:title="input.description")
           label.col-fixed(:for="'input_' + input.id" style="min-width: 150px; margin:auto")
             span {{ input.label }}
           .col
-            InputText.min-w-full(type="text", :id="'input_' + input.id", v-model="input.value")
+            template(v-if="input.type == 'text'")
+              InputText.min-w-full(type="text", :id="'input_' + input.id", v-model="input.value")
+            template(v-if="input.type == 'boolean'")
+              InputSwitch(v-model="input.value")
 </template>
 
 <style scoped>
