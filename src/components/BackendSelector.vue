34dd5312edcb46e44fa84b6835e8aa2ee0f2bb1c
Hanusz Leszek <leszek.hanusz@gmail.com>
2022-09-26 18:08:56 +0200

    Separate online and local backends in selector (#34)

--- src/components/BackendSelector.vue
+++ src/components/BackendSelector.vue
@@ -8,11 +8,18 @@ const backend = useBackendStore();
 
 <template lang="pug">
 .flex.flex-column.align-items-center
-  Dropdown#model(optionLabel="name", optionValue="id", v-model="backend.backend_id", :options="backend.backend_options")
+  Dropdown#model(optionLabel="label", optionValue="id", optionGroupLabel="label" optionGroupChildren="backends" v-model="backend.backend_id", :options="backend.backend_options")
 </template>
 
 <style scoped>
 label.col-fixed {
   width: 200px;
+}
+</style>
+
+<style>
+li.p-dropdown-item-group {
+  font-size: 20px;
+  text-decoration: underline;
 }
 </style>
