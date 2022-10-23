9113cda8f74f54e894a1cd383c216e30e71799c1
Hanusz Leszek <leszek.hanusz@gmail.com>
2022-10-23 23:20:22 +0200

    Fix image save If image is returned as a URL
    https://github.com/eligrey/FileSaver.js/issues/557

--- src/actions/save.js
+++ src/actions/save.js
@@ -1,14 +1,20 @@
 import FileSaver from "file-saver";
 import { useOutputStore } from "@/stores/output";
 
-function saveResultImage(image_index) {
+async function saveResultImage(image_index) {
   const output = useOutputStore();
 
-  const image_data = output.images.content[image_index];
+  let image_data = output.images.content[image_index];
   const fileextension = image_data.startsWith("data:image/webp;")
     ? ".webp"
     : ".png";
   const filename = output.images.metadata.input.prompt + fileextension;
+
+  // If the image_data is a URL instead of a blob, we first need to download it
+  if (image_data.startsWith("http")) {
+    const result = await fetch(image_data);
+    image_data = await result.blob();
+  }
 
   FileSaver.saveAs(image_data, filename);
 }
